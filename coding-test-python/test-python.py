f112179bc3f5b591c535dbdfcb1ba321dce18173
GIO\egbar <edwinbl@digitalware.com.co>
2022-03-30 18:13:16 -0500

    Ajsute error banco bogota test

--- coding-test-python/test-python.py
+++ coding-test-python/test-python.py
@@ -5,8 +5,14 @@
 # ** Date  : 18/05/2019       **
 # ** Author: ADL DevOps Team  **
 # ******************************
+
+
 from sys import exit
-from test.main import run
+
+import sys
+sys.path.append('/content/sample_data/test')
+
+import main
 
 if __name__ == "__main__":
-    exit(run())
+    exit(main.run())
--- coding-test-python/test/main.py
+++ coding-test-python/test/main.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-MAIN
-"""
+
+
 import json
 import requests
 
